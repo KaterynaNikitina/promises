afd47368794494ac8ed419ffc479b90629872a3d
Katerina <Kateryna.e.nikitina@gmail.com>
2023-11-17 20:32:00 -0700

    catch added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,44 @@ import { useEffect, useState } from "react";
 
 import "./App.css";
 
+function goodBadUrl() {
+  const PATH =
+    "learning-area/javascript/apis/fetching-data/can-store/products.json";
+  const GOOD_HOST = "mdn.github.io";
+  const BAD_HOST = "badhost.example.com";
+  const random = Math.random() * 2;
+  console.log(random);
+  const host = random < 1 ? BAD_HOST : GOOD_HOST;
+
+  return `https://${host}/${PATH}`;
+}
+
 function App() {
 
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
+
+    const controller = new AbortController();
+    const signal = controller.signal;
     console.log("useEffect starts");
     fetch(
-      "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+      goodBadUrl(), {signal: signal}
     )
       .then((response) => response.json())
-      .then((data) => setProducts(data));
+      .then((data) => setProducts(data))
+      .catch((error) => {
+        console.log(error);
+        if (error.name !== 'AbortError') {
+          setError(true);
+        }
+        
+      });
+
+      return () => {
+        controller.abort();
+      }
 
   }, []);
 console.log(products);
@@ -23,6 +50,11 @@ console.log(products);
     <>
       Hello World
       <div>
+      {error && ( 
+          <div>
+            The fetch did not work, there has been an error.
+          </div>
+        )}
         <ul>{list}</ul>
       </div>
     </>
